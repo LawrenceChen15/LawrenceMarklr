5ced29b99ba68911e1238c150be7ca5723367a45
LawrenceChen15 <lawrence.chn15@gmail.com>
2022-12-05 09:01:16 -0500

    Testing

--- frontEnd/testInput.php
+++ frontEnd/testInput.php
@@ -1,11 +1,10 @@
 <?php
 
-if (isset($_post["submit"]))
+if (isset($_POST["submit"]))
 {
     $email = $_POST["uname"];
     $pwd = $_POST["psw1"];
     $pwdRepeat = $_POST["psw2"];
     echo ("my user name is ");
 }
-
 ?>
